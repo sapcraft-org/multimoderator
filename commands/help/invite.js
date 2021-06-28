85c025cd8c36bee53accebd3b8eb4eb5414fa6fa
sapcraft-org <76895953+sapcraft-org@users.noreply.github.com>
2021-06-28 22:16:43 +0530

    Update invite.js

--- commands/help/invite.js
+++ commands/help/invite.js
@@ -3,14 +3,13 @@ const discord = require("discord.js");
 module.exports = {
   name: "invite",
   category: "info",
-  description: "INVITE Automodv12 beta BOT",
+  description: "INVITE YOUR MOM",
   run: async (client, message, args) => {
     
     let embed = new discord.MessageEmbed()
     .setTitle(`HERE INVITE LINK OF BOT`)
-    .setDescription(`<a:verify:743830477818953768>[CLICK HERE](https://discord.com/api/oauth2/authorize?client_id=744597377406599188&permissions=8&scope=bot) OR [support server ](https://discord.gg/RJ2SRN2cVu)`)
     .setColor("RANDOM")
-    .setFooter(`BOT MADE BY KHANMANAN#1000`)
+    .setFooter(`BOT MADE BY AUF(SAPPHIRE)`)
     .setTimestamp(message.timestamp = Date.now())
     
     message.channel .send(embed)
