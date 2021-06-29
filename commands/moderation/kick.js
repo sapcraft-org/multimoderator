06a086607f9417573841c018a66875a1372c04df
sapcraft-org <76895953+sapcraft-org@users.noreply.github.com>
2021-06-29 21:26:01 +0530

    fixed and updated

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,47 +1,54 @@
-const discord = require("discord.js");
+const { MessageEmbed } = require("discord.js")
+const moment = require('moment')
 
 module.exports = {
-  name: "kick",
-  category: "moderation",
-  description: "Kick anyone with one shot whithout knowing anyone xD",
-  usage: "kick <@user> <reason>",
-  run: async (client, message, args) => {
-    
-    const target = message.mentions.users.first() || message.guild.members.cache.get(args[0]).user
-    
-    const reason = args.slice(1).join(" ")
-    
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply(`You don't have enough powers to kick someone`)
-    
-    if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply(`I don't have powers to kick someone`)
-    
-    if(!args[0]) return message.reply(`Please mention someone to kick`)
-    
-    if(!target) return message.reply(`I can't find that member`)
-    
-    if(target.roles.highest.position >= message.author.roles.highest.position || message.author.id !== message.guild.owner.id) {
-      return message.reply(`They have more power than you`)
+name: "kick",
+category: "moderation",
+description: "kick a user",
+cooldown: 5,
+userPerms: ["KICK_MEMBERS"],
+clientPerms: ["KICK_MEMBERS"],
+run: async(client, message, args) => {  
+const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+const reason = args.slice(1).join(" ")
+          if (!args[0]) return message.channel.send(":x: | **Specify someone to kick.**")
+        if (!mentionedMember) return message.channel.send(":x: | **I can't find that member.**")
+        if (mentionedMember.id === message.author.id) return message.channel.send(":x: | You can't kick yourself.")
+        if (mentionedMember.roles.highest.position >= message.member.roles.highest.position && message.author.id !== message.guild.owner.id) {
+            return message.channel.send(":x: | **You can\'t kick this member due to your role being lower than that member role.**")
+        }
+        if (mentionedMember.kickable) {
+            const embed = new MessageEmbed()
+            .setAuthor(`${message.author.username} - (${message.author.id})`, message.author.displayAvatarURL({dynamic: true}))
+            .setThumbnail(mentionedMember.user.displayAvatarURL({dynamic: true}))
+            .setColor(`RANDOM`)
+            .setDescription(`
+**Member:** ${mentionedMember.user.username} - (${mentionedMember.user.id})
+**Reason:** ${reason || "None"}
+            `)
+        message.channel.send(embed)
+        mentionedMember.kick()
+        } else {
+            return message.channel.send(":x: | **I can\'t kick this user make sure that the users role is lower than my role.**")
+        }
+        return undefined
+    let channel = db.fetch(`modlog_${message.guild.id}`)
+            if (!channel) return;
+
+            const embed = new MessageEmbed()
+                .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
+                .setColor("#ff0000")
+                .setThumbnail(kickMember.user.displayAvatarURL({ dynamic: true }))
+                .setFooter(message.guild.name, message.guild.iconURL())
+                .addField("**Moderation**", "kick")
+                .addField("**User Kicked**", kickMember.user.username)
+                .addField("**Kicked By**", message.author.username)
+                .addField("**Reason**", `${reason || "**No Reason**"}`)
+                .addField("**Date**", message.createdAt.toLocaleString())
+                .setTimestamp();
+
+            var sChannel = message.guild.channels.cache.get(channel)
+            if (!sChannel) return;
+            sChannel.send(embed)
+        }
     }
-    
-    if(target.id === message.author.id) return message.reply(`I can't kick you as you are the Boss`)
-    
-    if(target.kickable) {
-      let embed = new discord.MessageEmbed()
-      .setAuthor(target.username.tag, target.avatarURL({dynamic: true, size: 1024}))
-      .setColor("RANDOM")
-      .setDescription(
-        `
-**❯ User:** ${target}
-**❯ Action:** Kick
-**❯ Reason:** \`${reason || "No Reason Provided"}\``)
-      
-      message.channel.send(embed)
-      
-      target.kick()
-      
-    } else {
-      return message.reply(`I can't kick them, make sure that my role is above of theirs`)
-    }
-    return undefined
-  }
-};
